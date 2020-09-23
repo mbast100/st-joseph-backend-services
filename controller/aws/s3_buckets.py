81df50b65b365b91b449faf9989d6169d29c35eb
Mark Bastawros <mark.bastawros@shopify.com>
2020-09-22 23:18:07 -0400

    updated media image upload to s3 buckets

--- controller/aws/s3_buckets.py
+++ controller/aws/s3_buckets.py
@@ -5,24 +5,31 @@ from api_exception import ApiException
 
 
 class S3Bucket():
-    def __init__(self):
+    def __init__(self, filename = ''):
         self.s3_client = boto3.client('s3')
         self.s3_resource = boto3.resource('s3')
         self.response = {}
+        self.file_name = filename
         self.message ={}
+        self.image_url_seaonsal = "https://st-joseph-seasonal.s3.amazonaws.com/{}/{}"
+        self.image_url = "https://st-joseph-media.s3.amazonaws.com/{}/{}"
+        self.response_message ={
+            "success" : "uploaded {} to {}"
+        }
 
-    def upload_file(self,bucket, filename, file):
+    def upload_file(self,bucket, filename, file, path=''):
         try:
-            self.response = self.s3_resource.Bucket(bucket).put_object(Key=filename,Body=file)
+            key = filename
+            if path:
+                key = path+"/"+filename
+            self.response = self.s3_resource.Bucket(bucket).put_object(Key=key,Body=file)
         except Exception as e:
-            print(e)
             raise ApiException("something went wrong while uploading to s3 bucket", 400)
     
     def delete_file(self, bucket, key):
         try:
             self.response = self.s3_client.delete_object(Bucket = bucket, Key =key)
         except Exception as e:
-            print(e)
             raise ApiException("something went wrong while deleting o s3 bucket", 400)
 
     def list_files(self,bucket):
--- routes/media.py
+++ routes/media.py
@@ -16,9 +16,20 @@ def media():
     elif request.method == "POST":
         try:
             file_to_upload = request.files["image"]
-            s3.upload_file(args.get("bucket"),file_to_upload.filename, file_to_upload)
-            return jsonify({"message":"uploaded {} to {}".format(file_to_upload.filename, args.get("bucket"))}), 200
+            if args.get("type") == "seasonal":
+                s3.upload_file("st-joseph-seasonal",file_to_upload.filename, file_to_upload, path=args.get("month"))
+                return jsonify({
+                    "message":s3.response_message.get("success").format(file_to_upload.filename,"st-joseph-seasonal"),
+                    "image":s3.image_url_seaonsal.format(args.get("month"),file_to_upload.filename)
+                    }), 200
+            else:
+                s3.upload_file("st-joseph-media",file_to_upload.filename, file_to_upload,path=args.get("type"))
+                return jsonify({
+                    "message":s3.response_message.get("success").format(file_to_upload.filename,"st-joseph-media"),
+                    "image":s3.image_url.format(args.get("type"), file_to_upload.filename)
+                    }), 200
         except Exception as e:
+            print(e)
             return jsonify({"error":e.__dict__.get("message")}), 500
 
     elif request.method == "DELETE":
