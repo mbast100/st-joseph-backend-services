af772337eb03eea8237a1f1136e226b56704eb95
Mark Bastawros <mark.bastawros@shopify.com>
2020-11-26 17:14:59 -0500

    added test for media

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 END_POINT = "https://zq4r9erb58.execute-api.us-east-1.amazonaws.com/prod/api/{}"
 @pytest.fixture(scope='session')
 def prod_endpoint():
@@ -10,6 +11,9 @@ def prod_endpoint():
 def media_endpoint():
     return END_POINT.format("media")
 
+@pytest.fixture(scope="session")
+def endpoint():
+    return END_POINT
 
 @pytest.fixture(scope='session')
 def internal_configuration_endpoint():
--- tests/routes/test_prod_media.py
+++ tests/routes/test_prod_media.py
@@ -3,22 +3,26 @@ import sys
 
 class TestProdMedia():
 
-    def test_get_media_by_bucket(self, media_endpoint):
-        web = HTTPcore(media_endpoint)
-        web.core("GET", params={"bucket":"st-joseph-media"})
-        assert web.get_status_code == 200
+    web = HTTPcore("https://zq4r9erb58.execute-api.us-east-1.amazonaws.com/prod/api/media")
 
-    def test_get_media_by_without_bucket_name(self, media_endpoint):
-        web = HTTPcore(media_endpoint)
-        web.core("GET")
-        assert web.get_status_code == 400
-        assert web.json_response["message"] == "missing query param 'bucket'"
+    def test_get_media_by_bucket(self):
+        self.web.core("GET", params={"bucket":"st-joseph-media"})
+        assert self.web.get_status_code == 200
 
-    def test_get_all_items_with_prefix(self, media_endpoint):
-        web = HTTPcore(media_endpoint)
+    def test_get_media_by_without_bucket_name(self):
+        self.web.core("GET")
+        assert self.web.get_status_code == 400
+        assert self.web.json_response["message"] == "missing query param 'bucket'"
+
+    def test_get_all_items_with_prefix(self):
         prefix ="schedule"
-        web.core("GET", params={"bucket": "st-joseph-media", "prefix": prefix})
-        assert web.get_status_code == 200
-        for item in web.json_response:
+        self.web.core("GET", params={"bucket": "st-joseph-media", "prefix": prefix})
+        assert self.web.get_status_code == 200
+        for item in self.web.json_response:
             assert prefix in item["Key"]
             assert "url" in item
+
+    def test_missing_bucket_name(self):
+        self.web.core("GET", params={"bucket": ""})
+        assert self.web.get_status_code == 400
+        assert self.web.json_response["message"] == "missing query param 'bucket'"
